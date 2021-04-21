25fa9483d7a094c7bc820d3652e57334151bb541
jenishj123 <37879477+jenishj123@users.noreply.github.com>
2021-04-20 21:04:39 -0400

    new app.py

--- app.py
+++ app.py
@@ -32,6 +32,55 @@ def welcome():
     print(results.head().to_json(orient="records"))
     
     return jsonify(results.head().to_dict(orient="records"))
+
+@app.route("/api/v1.0/name")
+def name():
+    
+    """Retreive Meteorite Landings Data"""
+    session = Session(engine)
+
+    """Return a list of name"""
+    # Query for the dates and precipitation from last year
+    name = session.query(Landings.name).all()
+    return jsonify(name)
+
+
+@app.route("/api/v1.0/latitude")
+def latitude():
+    
+    """Retreive Meteorite Landings Data"""
+    session = Session(engine)
+
+    """Return a list of latitude"""
+    # Query for the dates and precipitation from last year
+    latitude = session.query(Landings.latitude).all()
+    return jsonify(latitude)
+
+
+@app.route("/api/v1.0/longitude")
+def longitude():
+    
+    """Retreive Meteorite Landings Data"""
+    session = Session(engine)
+
+    """Return a list of longitude"""
+    # Query for the dates and precipitation from last year
+    longitude = session.query(Landings.longitude).all()
+    return jsonify(longitude)
+
+
+@app.route("/api/v1.0/mass")
+def mass():
+    
+    """Retreive Meteorite Landings Data"""
+    session = Session(engine)
+
+    """Return a list of mass"""
+    # Query for the dates and precipitation from last year
+    mass = session.query(Landings.mass).all()
+    return jsonify(mass)
+
+
     
 if __name__ == '__main__':
         app.run()
